6a2f5c240742876139d97e408f2d2c265f72aa86
Daniel Westendorf <daniel@prowestech.com>
2025-12-06 13:51:31 -0700

    Add bucket store

---
+++ lib/specwrk/store/bucket.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require "specwrk/store/base"
+
+module Specwrk
+  class Bucket < Store
+    EXAMPLES_KEY = :____examples
+
+    def examples=(val)
+      @examples = nil
+
+      self[EXAMPLES_KEY] = if val.nil? || val.length.zero?
+        nil
+      else
+        val
+      end
+    end
+
+    def examples
+      @examples ||= self[EXAMPLES_KEY] || []
+    end
+
+    def clear
+      @examples = nil
+      super
+    end
+
+    def reload
+      @examples = nil
+      super
+    end
+  end
+end
